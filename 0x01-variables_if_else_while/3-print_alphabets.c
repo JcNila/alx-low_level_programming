e61b6502c6459cf8a606fae8526c34bb40c48b3e
JcNila <achiengcindyy@gmail.com>
2022-04-10 04:53:59 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <studlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char low;
+	for (low = 'a' ; low <= 'z' ; low++)
+		putchar(low);
+	for (low = 'A' ; low <= 'Z' ; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
